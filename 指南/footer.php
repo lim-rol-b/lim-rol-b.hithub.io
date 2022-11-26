4839cf62bf771bc796a17a97478225b8780790a3
lim <78190125+lim-rol-b@users.noreply.github.com>
2022-11-26 22:53:12 +0800

    Add files via upload

---
+++ 指南/footer.php
@@ -0,0 +1,102 @@
+
+<!-- bloc-1 -->
+<div class="bloc l-bloc" id="bloc-1">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col text-md-left text-center">
+				<h2 class="mg-md">
+					Part 0
+				</h2>
+				<p>
+					在开始之前，我为您准备了一瓶水，您可以低下头，您应该很容易看到它正在在书挂上。
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-1 END -->
+
+<!-- bloc-2 -->
+<div class="bloc l-bloc" id="bloc-2">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col text-md-left text-center">
+				<h2 class="mg-md">
+					Part 1
+				</h2>
+				<h3 class="mg-md">
+					这部分旨在帮助您了解罗一铭同学与班级的关系，并且对班级有一个较好的认识<br>
+				</h3>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-2 END -->
+
+<!-- bloc-3 -->
+<div class="bloc l-bloc" id="bloc-3">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 text-md-left text-center">
+				<h4 class="mg-md">
+					我们来认识一下附近的同学<br>
+				</h4>
+				<p>
+					依照罗一铭同学目前的座位来看，坐在他左边的是施宇豪同学（同时也是罗一铭同学的室友），坐在他右边的是余凡同学；施宇豪后面坐的是王瑞雪同学；罗一铭后面坐的的是程曦锐同学；余凡后面坐的是陈柯行（同样，也是罗一铭同学的室友）<br>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-3 END -->
+
+<!-- bloc-4 -->
+<div class="bloc l-bloc" id="bloc-4">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 text-md-left text-center">
+				<h2 class="mg-md">
+					Part 2
+				</h2>
+				<h3 class="mg-md">
+					罗一铭给您的信
+				</h3>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-4 END -->
+
+<!-- bloc-5 -->
+<div class="bloc l-bloc" id="bloc-5">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 text-md-left text-center">
+				<p>
+					毕竟是期中考后的家长会，难免要谈到成绩，那么谈就谈吧。<br>期中考成绩很不理想，这是事实，我在此也不再过多赘述，毕竟我们在家中谈的已经够多了。总之我会在语文英语方面努力，并且在九校联考中取得我想要的成绩。Z9志在必得！<br>您可能会觉得我写的不够多，那么，再加上些未来的计划。<br>考入年级150名，暂且就这么定了吧！<br>
+				</p>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-5 END -->
+
+<!-- bloc-6 -->
+<div class="bloc l-bloc" id="bloc-6">
+	<div class="container bloc-lg">
+		<div class="row">
+			<div class="col-sm-10 offset-sm-1 col-md-8 offset-md-2 col-lg-6 offset-lg-3 text-md-left text-center">
+				<h2 class="mg-md">
+					该指南到此结束
+				</h2>
+				<h3 class="mg-md">
+					Thank you for reading
+				</h3>
+				<h4 class="mg-md">
+					by 罗一铭同学
+				</h4>
+			</div>
+		</div>
+	</div>
+</div>
+<!-- bloc-6 END -->
